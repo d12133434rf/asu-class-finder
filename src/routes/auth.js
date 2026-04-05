1d3c2b4fcab159e8f9eac7b235bc25eca41749ed
d12133434rf <fninja682@gmail.com>
2026-04-04 18:19:37 -0700

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,8 +3,8 @@ const express = require("express");
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: 'smtp-relay.brevo.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS
